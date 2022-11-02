8d3770282a4197996b7481ada751e3741e52f85d
SelfJ19 <SELFJ1@ETSU.EDU>
2022-11-02 13:10:56 -0400

    added comments to the Enum.cs

--- Lab6/Enum.cs
+++ Lab6/Enum.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Lab6
 {
+    /// <summary>
+    /// creates the public enum DamageType
+    /// </summary>
     public enum DamageType
     {
         Slashing,
