fc36f54758c46e1fb8604d1b396ece308030c466
SelfJ19 <SELFJ1@ETSU.EDU>
2022-11-03 11:24:56 -0400

    finished Clue.cs with all comments

--- Lab6/Clue.cs
+++ Lab6/Clue.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Project: Lab 6 Inheritance
+// File Name: Consumable.cs
+// Description: Represents the Consumable Object
+// Course: CSCI 1260-001 – Introduction to Computer Science II
+// Author: Jason Self, selfj1@etsu.edu, East Tennessee State University
+// Created: 11/02/2022
+// Copyright: Jason Self, 2022
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,15 +18,32 @@ using System.Threading.Tasks;
 
 namespace Lab6
 {
+    /// <summary>
+    /// Creates the class Clue which is a subclass of Item
+    /// </summary>
     public class Clue : Item
     {
+        #region Attributes
+        /// <summary>
+        /// Attributes - data that belongs to and describes an object of the class
+        /// </summary>
         private string PersonInciminated { get; set; }
         private bool Accuracy { get; set; }
+        #endregion
 
+        #region Parameterized()
+        /// <summary>
+        /// Parameterized Constructor that also uses the Item parameterized constructor
+        /// </summary>
+        /// <param name="name">used to set Name from the parent class Item</param>
+        /// <param name="description">used to set Description from the parent class Item</param>
+        /// <param name="weight">used to set Weight from the parent class Item</param>
+        /// <param name="value">used to set Value from the parent class Item</param>
         public Clue(string name, string description, double weight, double value) : base(name, description, weight, value)
         {
             PersonInciminated = "";
             Accuracy = false;
         }
+        #endregion
     }
 }
